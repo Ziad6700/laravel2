f0bb0bae5adf55c16e54dd7ea2411b946cd035e0
jaydenlaunuru <9025846@student.zadkine.nl>
2026-01-15 13:34:23 +0100

    wachtwoord hidden/show function fixed

--- resources/views/registreren.blade.php
+++ resources/views/registreren.blade.php
@@ -73,7 +73,7 @@
                 <input
                     class="block w-full bg-gray-50 border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-formHover-green-600 focus:border-formHover-green-600"
                     type="password"
-                    name="wachtwoord"
+                    name="password"
                     placeholder="password90"
                     required
                 >
